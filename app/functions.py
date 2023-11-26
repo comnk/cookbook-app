06b63bd30d1ac9ab7538dde6b7b7e69bc0526238
comnk <vhsiao36@gmail.com>
2023-11-25 18:40:17 -0800

    day 3 updates - added more features such as one to remove recipe and cookbook pdf files and a feature to extract a recipe from a website, debugged and fixed some other features

--- app/functions.py
+++ app/functions.py
@@ -1,12 +1,145 @@
 from dotenv import load_dotenv
-import requests
-import pdfkit
-import os
+from os import listdir
+from os.path import isfile, join
+from pypdf import PdfWriter
+import glob, os, pdfkit, requests, random, string
 
 load_dotenv()
 api_key = os.getenv("API_KEY")
 
+# GENERAL FEATURES
+
+def clear_history():
+    history_input = input("Which history would you like to clear? ")
+
+    match history_input:
+        case "recipe":
+            files = glob.glob('../recipe-docs/recipes/*.pdf')
+
+            if (len(files) == 0):
+                print("No files to clear!")
+
+            for f in files:
+                os.remove(f)
+            
+            print("All recipe files removed")
+
+        case "cookbook":
+            files = glob.glob('../recipe-docs/cookbooks/*.pdf')
+
+            if (len(files) == 0):
+                print("No files to clear!")
+
+            for f in files:
+                os.remove(f)
+            
+            print("All cookbook files removed")
+
+        case "_":
+            print("Invalid input! Please try again!")
+
+def save_pdf(title, image, ingredients, instructions):
+    print("Ok! Saving pdf to your computer")
+
+    file_title = title.lower().replace(" ", "")
+    path = "../recipe-docs/recipes/" + file_title + '.html'
+    
+    with open(path, 'w') as f:
+        f.write(f"<h1>{title}</h1>\n")
+        f.write(f"<img src={image} width='300' height='300'>\n")
+        f.write("<ul>")
+
+        for ingredient in ingredients:
+            f.write(f"<li>{ingredient['original']}</li>\n")
+
+        f.write("</ul>")
+        f.write(instructions)
+        f.close()
+                
+    pdfkit.from_file(path, "../recipe-docs/recipes/" + file_title + '.pdf') 
+    print("PDF saved!")
+
+    os.remove(path)
+
+# FOOD FEATURES FOR CHATBOT
+
+def extract_recipe():
+
+    """
+    
+    """
+
+    get_website = input("Insert website url here: ")
+
+    endpoint = "https://api.spoonacular.com/recipes/extract"
+    params = {"apiKey": api_key, "url": get_website}
+    response = requests.get(endpoint, params=params)
+
+    # Check the status code of the response
+    if response.status_code == 200:
+        # Parse the JSON response
+        recipe = response.json()
+        # Extract information about the random recipe
+        try:
+            title = recipe["title"]
+            ingredients = recipe["extendedIngredients"]
+            instructions = recipe["instructions"]
+
+            # Display information
+            print(f"Title: {title}")
+            print("Ingredients:")
+            for ingredient in ingredients:
+                print(f"  - {ingredient['original']}")
+            print("Instructions:")
+            print(instructions)
+
+            save_input = input("Do you want to save this recipe? ").lower()
+
+            if (save_input == "yes"):
+                save_pdf(title, recipe["image"], ingredients, instructions)
+
+            elif (save_input == "no"):
+                print("Ok! Heading back to home page!")
+            else:
+                print("Invalid input!")
+
+        except IndexError:
+            print(f"Sorry that url does not exists! Please try again!")
+
+    else:
+        print(f"Failed to retrieve a recipe. Status code: {response.status_code}")
+
+def generate_cookbook():
+
+    """
+    
+    """
+
+    recipe_files = [f for f in listdir("../recipe-docs/recipes/") if isfile(join("../recipe-docs/recipes/", f))]
+
+    if (len(recipe_files) == 0):
+        print("No files to merge! Please upload recipes!")
+    else:
+        print("Merging...")
+        merge = PdfWriter()
+
+        for pdf in recipe_files:
+            merge.append("../recipe-docs/recipes/" + pdf)
+
+        all_char = string.ascii_letters + string.punctuation + string.digits
+        password = "".join(random.choice(all_char) for x in range(random.randint(1, 10)))
+
+        merge.write("../recipe-docs/cookbooks/cookbook" + password + ".pdf")
+        merge.close()
+
+        print("Merge successful!")
+
 def random_recipe():
+
+    """
+    
+    """
+
     cuisine = input("What cuisine are you feeling today? ")
     type_meal = input("What type of meal do you want? ")
     endpoint = "https://api.spoonacular.com/recipes/random"
@@ -36,24 +169,7 @@ def random_recipe():
             save_input = input("Do you want to save this recipe? ").lower()
 
             if (save_input == "yes"):
-                print("Ok! Saving pdf to your computer")
-
-                file_title = title.lower().replace(" ", "")
-                path = "../recipe-docs/" + file_title + '.html'
-                with open(path, 'w') as f:
-                    f.write(f"<h1>{title}</h1>\n")
-                    f.write("<ul>")
-                    for ingredient in ingredients:
-                        f.write(f"<li>{ingredient['original']}</li>\n")
-
-                    f.write("</ul>")
-                    f.write(instructions)
-                    f.close()
-                
-                pdfkit.from_file(path, "../recipe-docs/" + file_title + '.pdf') 
-                print("PDF saved!")
-
-                os.remove(path)
+                save_pdf(title, recipe["image"], ingredients, instructions)
 
             elif (save_input == "no"):
                 print("Ok! Heading back to home page!")
--- app/main.py
+++ app/main.py
@@ -1,11 +1,17 @@
-from functions import random_recipe
+from functions import *
 
 while True:
     initial_input = input("Welcome back! What would you like to do? ")
 
     match initial_input:
+        case "clear history":
+            clear_history()
+        case "extract recipe":
+            extract_recipe()
         case "random recipe":
             random_recipe()
+        case "generate cookbook":
+            generate_cookbook()
         case "exit":
             print("Thanks for using the app! Have a good day!")
             break
